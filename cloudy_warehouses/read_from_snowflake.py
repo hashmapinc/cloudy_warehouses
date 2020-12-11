fb4455f8a037cea3422f11083a10e7bbef1ecf37
samkohlleffel <sam.kohlleffel@gmail.com>
2020-12-11 10:38:39 -0600

    combined read/list into one object

--- cloudy_warehouses/read_from_snowflake.py
+++ cloudy_warehouses/read_from_snowflake.py
@@ -1,74 +1,77 @@
 import yaml, os
+from cloudy_warehouses.snowflake_objects.snowflake_object import SnowflakeObject
 from snowflake import connector
 
 
 # Reads from Snowflake and returns pandas dataframe
-class SnowflakeReader:
+class SnowflakeReader(SnowflakeObject):
 
-    def read(database: str, schema: str, table: str, sf_username: str=None, sf_password: str=None, sf_account: str=None):
-
-        # configures Snowflake credentials
-        def configure_credentials(sf_username: str=None, sf_password: str=None, sf_account: str=None):
-
-            # determines where to look for Snowflake credentials
-            if not sf_username or not sf_password or not sf_account:
-                # Path to Snowflake credentials file
-                __profile_path: str = os.path.join(os.getenv("CLOUDY_HOME"), '.cloudy_warehouses/configuration_profiles.yml')
-
-                with open(__profile_path, 'r') as stream:
-                    sf_credentials = yaml.safe_load(stream)['profiles']['snowflake']
-
-            else:
-                # uses passed in variables as Snowflake credentials
-                sf_credentials = {
-                    'user': sf_username, 
-                    'pass': sf_password, 
-                    'acct': sf_account
-                    }
-                
-            return sf_credentials
-
-        # establishes a connection with snowflake
-        def get_snowflake_connection(user, pswd, acct, database=None, schema=None):
-            connection = connector.connect(
-                user = user,
-                password = pswd,
-                account = acct,
-                database = database,
-                schema = schema
-            )
-
-            return connection
-
-        # reads data from a Snowflake table as a pandas dataframe
-        def get_pandas_dataframe(connection, database, schema, table):
-            try:
-                cursor = connection.cursor()
-                cursor.execute(f'select * from {database}.{schema}.{table}')
-                df = cursor.fetch_pandas_all()
-            finally:
-                cursor.close()
-
-            return df
+    def read(self, database: str, schema: str, table: str, sf_username: str = None, sf_password: str = None, sf_account: str = None):
 
         # calls function to configure Snowflake credentials
-        sf_credentials = configure_credentials(
+        sf_credentials = self.configure_credentials(
             sf_username=sf_username,
             sf_password=sf_password,
             sf_account=sf_account
             )
         
         # calls function to connect to Snowflake using the sf_credentials variable
-        connection = get_snowflake_connection(
+        connection = self.get_snowflake_connection(
             user=sf_credentials['user'],
             pswd=sf_credentials['pass'],
             acct=sf_credentials['acct']
             )
         
         # calls function to return data in a Snowflake table as a pandas dataframe
-        return get_pandas_dataframe(connection=connection,
-                                    database=database,
-                                    schema=schema,
-                                    table=table
-                                    )
+        return self.get_pandas_dataframe(
+            connection=connection,
+            database=database,
+            schema=schema,
+            table=table
+            )
+
+    def list_tables(self, database: str, sf_username: str = None, sf_password: str = None, sf_account: str = None):
+        # calls function to configure Snowflake credentials
+        sf_credentials = self.configure_credentials(
+            sf_username=sf_username,
+            sf_password=sf_password,
+            sf_account=sf_account
+        )
+
+        # calls function to connect to Snowflake using the sf_credentials variable
+        connection = self.get_snowflake_connection(
+            user=sf_credentials['user'],
+            pswd=sf_credentials['pass'],
+            acct=sf_credentials['acct']
+        )
+
+        # calls function to return data in a Snowflake table as a pandas dataframe
+        return self.get_snowflake_tables(
+            connection=connection,
+            database=database
+            )
+
+    # reads data from a Snowflake table as a pandas dataframe
+    def get_pandas_dataframe(self, connection, database, schema, table):
+        try:
+            cursor = connection.cursor()
+            cursor.execute(f'select * from {database}.{schema}.{table}')
+            df = cursor.fetch_pandas_all()
+        finally:
+            cursor.close()
+
+        return df
+
+    # reads data from a Snowflake table as a pandas dataframe
+    def get_snowflake_tables(self, connection, database):
+
+        try:
+            cursor = connection.cursor()
+            cursor.execute(
+                f"SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME FROM {database}.INFORMATION_SCHEMA.TABLES;")
+            tables = cursor.fetch_pandas_all()
+        finally:
+            cursor.close()
+
+        return tables
 
