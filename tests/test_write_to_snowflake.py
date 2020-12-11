9a2b44a1a807702f107bb0d092a0c9a7563f6d4d
samkohlleffel <sam.kohlleffel@gmail.com>
2020-12-11 12:46:23 -0600

    updated tests

--- tests/test_write_to_snowflake.py
+++ tests/test_write_to_snowflake.py
@@ -7,14 +7,14 @@ class TestWriteSnowflake(unittest.TestCase):
 
     def test_write_snowflake(self):
 
-        df = pd.DataFrame.from_dict({'V': ['B', 'O', 'O', 'M']})
+        df = pd.DataFrame.from_dict({'COL_1': ['hello', 'there'], 'COL_2': [10, 20]})
 
         expected_return_statement = 'successfully wrote to Snowflake Table'
 
         return_statement = df.cloudy_warehouses.write_snowflake(
             database='TEST_DB',
             schema='PANDAS_CLOUDY_TEST',
-            table_name='TO_SNOWFLAKE_TEST'
+            table='TO_SNOWFLAKE_TEST'
         )
 
         self.assertEqual(
