0b6739490a7351dbcc8abc9832cec8566387634c
samkohlleffel <sam.kohlleffel@gmail.com>
2020-12-16 11:34:44 -0600

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cloudy_warehouses",
-    version="0.0.3.7",
+    version="0.0.3.8",
     author="Hashmap, Inc",
     author_email="accelerators@hashmapinc.com",
     description="DO NOT USE - This is a sample program",
